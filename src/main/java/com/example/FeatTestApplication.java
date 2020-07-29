6d7af8c0e8f96a4bbbad7244d49e9c71f24ee879
Wang Zhiqiang <wangzhiqiang@eastmoney.com>
2020-07-29 10:57:54 +0800

    Initial commit

---
+++ src/main/java/com/example/FeatTestApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wang.zhiqiang
+ * @version 1.0
+ * @date 2020/7/29 10:38
+ */
+@SpringBootApplication
+public class FeatTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatTestApplication.class, args);
+    }
+}
