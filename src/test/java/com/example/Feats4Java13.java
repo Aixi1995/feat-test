fce28122fc3e6a556ce0adce19468c9e9f40d3c0
Wang Zhiqiang <1304004930@qq.com>
2020-07-30 16:47:15 +0800

    test: java13 text blocks

---
+++ src/test/java/com/example/Feats4Java13.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.junit.Test;
+
+/**
+ * @author wang.zhiqiang
+ * @version 1.0
+ * @date 2020/7/30 16:18
+ */
+public class Feats4Java13 {
+
+    /**
+     * java13 引入的多行文本块, 现在在代码里写xml，html或者json等文本要方便多了
+     */
+    @Test
+    public void testMultiLineTextBlocks() {
+        var str = """
+                1
+                2
+                3
+                """;
+        System.out.println(str);
+    }
+
+    //switch socket...
+}
