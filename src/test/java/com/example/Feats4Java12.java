dc39690a8ce6dc7eb3c4939d837a149406abbcf8
Wang Zhiqiang <1304004930@qq.com>
2020-07-30 16:46:57 +0800

    test: java12 Switch expression

---
+++ src/test/java/com/example/Feats4Java12.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.junit.Test;
+
+/**
+ * @author wang.zhiqiang
+ * @version 1.0
+ * @date 2020/7/30 16:05
+ */
+public class Feats4Java12 {
+
+    /**
+     * java12 switch可以有返回值了
+     */
+    @Test
+    public void testSwitch() {
+        var grade = "A";
+        String _grade = switch (grade) {
+            case "A" -> "优秀";
+            case "B" -> "良好";
+            case "C", "D" -> "继续努力";
+            default -> "unknown";
+        };
+        System.out.println(_grade);
+    }
+}
