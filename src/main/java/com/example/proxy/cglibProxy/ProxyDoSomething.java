28e5e33464a52ef6b3784c0bf481945c9a490218
Wang Zhiqiang <1304004930@qq.com>
2020-08-04 10:38:33 +0800

    test: design pattern for CGlib proxy

---
+++ src/main/java/com/example/proxy/cglibProxy/ProxyDoSomething.java
@@ -0,0 +1,39 @@
+package com.example.proxy.cglibProxy;
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author wang.zhiqiang
+ * @version 1.0
+ * @date 2020/8/4 10:29
+ */
+public class ProxyDoSomething implements MethodInterceptor {
+
+    private Enhancer enhancer = new Enhancer();
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        doBefore();
+        Object object = methodProxy.invokeSuper(o, objects);
+        doAfter();
+        return null;
+    }
+
+    public Object getProxy(Class clazz) {
+        enhancer.setSuperclass(clazz);
+        enhancer.setCallback(this);
+        return enhancer.create();
+    }
+
+    private void doBefore() {
+        System.out.println("do before...");
+    }
+
+    private void doAfter() {
+        System.out.println("do after...");
+    }
+}
---
+++ src/test/java/com/example/designPattern/CglibProxyTest.java
@@ -0,0 +1,23 @@
+package com.example.designPattern;
+
+import com.example.proxy.DoSomethingA;
+import com.example.proxy.cglibProxy.ProxyDoSomething;
+import org.junit.Test;
+
+/**
+ * @author wang.zhiqiang
+ * @version 1.0
+ * @date 2020/8/4 10:35
+ */
+public class CglibProxyTest {
+
+    /**
+     * 利用cglib实现动态代理，相比于jdk的动态代理，cglib可以代理没有实现接口的类，实现也比jdk动态代理简单
+     */
+    @Test
+    public void testCglibTest() {
+        ProxyDoSomething proxyDoSomething = new ProxyDoSomething();
+        DoSomethingA doSomethingA = (DoSomethingA) proxyDoSomething.getProxy(DoSomethingA.class);
+        doSomethingA.doSomething();
+    }
+}
